eab06c054cbc853c363716fdbfacb9771f6f536a
Asif Ahmed <asif@stitchdesignco.com>
2017-02-09 17:57:13 -0500

    replace divs wiht html5 tags

--- content/themes/bhdDynamic/index.php
+++ content/themes/bhdDynamic/index.php
@@ -35,42 +35,43 @@
 <!-- ====  Section: About  ==== -->
 <section id="about">
 	<div>01</div>
-	<div>
+	<article>
 		<h1>In the <span>Heart</span> of Houston</h1>
 		<p>Buffalo Heights is a high quality, mixed-use district ideally located in the proximity to Houston's Buffalo Bayou Park, Memorial Park, the Heights, Washington Avenue Corridor and Downtown.</p>
-	</div>
+	</article>
   <div>
 	  <div>Seal</div>
 		<img class="" id="" src="<?php echo $GLOBALS['url']."/assets/img-02-mobile.png"; ?>" alt="" />
 	</div>
-	<div>
+	<article>
 		<img class="" id="" src="<?php echo $GLOBALS['url']."/assets/img-01-mobile.png"; ?>" alt="" />
-		<h1><span>1500</span><span>+</span> Acres of urban parkland</h1>
+		<h1><span>1500</span><sup>+</sup> Acres of urban parkland</h1>
 		<p>This innovative real estate development will offer a complete lifestye built around outdoor activity, healthy local food, and signature greenspace in the region's most desirable and accessible urban infill location. New development and upgrades to existing buildings will enhance proximity to the trails and bayou, and more than 1,500 acres of parkland in the urban core of the city</p>
-  </div>
-	<div>
+  </article>
+	<blockquote>
 		<h1>There is no location like this one in Houston</h1>
 		<h2>Russel Gordy Owner of BKR Memorial II, LLC</h2>
-	</div>
-	<div>
+	</blockquote>
+	<article>
 	  <img class="" id="" src="<?php echo $GLOBALS['url']."/assets/img-04-mobile.png"; ?>" alt="" />
 	  <h1>Buffalo Heights <span>Phase I</span></h1>
 	  <p>The First phaze of Buffalo Heights is a mixed-use Project on Approximately 4 acres at th intersectio of Washington Avenue and South Heights Blvd. The project includes a new urban prototype H-E-B, multi-family, offices and retail.</p>
-  </div>
+  </article>
 </section>
 
 <!-- ====  Section: Pres  ==== -->
 <section id="press">
   <div>02</div>
-  <div>
+  <blockquote>
 		<h1>Quote from article quote from Article</h1>
 		<div>Houston Chronicle
 		January 2017</div>
+	</div>	
 	<div>
 	  <a href="#" alt="">Read More</a>
 	</div>
   <div>
-    <div>
+    <article>
       <div>
         Houston Chronicle
         January 2017
@@ -78,8 +79,8 @@
       <div>
         <a href="#" alt="">Read More</a>
       </div>
-    </div>
-        <div>
+    </article>>
+    <article>
       <div>
         Houston Chronicle
         January 2017
@@ -87,8 +88,8 @@
       <div>
         <a href="#" alt="">Read More</a>
       </div>
-    </div>
-    <div>
+    </article>>
+    <article>
       <div>
         Houston Chronicle
         January 2017
@@ -96,8 +97,8 @@
       <div>
         <a href="#" alt="">Read More</a>
       </div>
-    </div>
-    <div>
+    </article>>
+    <article>
       <div>
         Houston Chronicle
         January 2017
@@ -105,7 +106,7 @@
       <div>
         <a href="#" alt="">Read More</a>
       </div>
-    </div>
+    </article>
   </div>
   <img class="" id="" src="<?php echo $GLOBALS['url']."/assets/img-05-mobile.png"; ?>" alt="" />
 </section>
@@ -125,10 +126,10 @@
 	<div>04</div>
 	<div>
 	  <h1>Connect</h1>
-	  <div>
-	    555 123 4567
-	    info@buffaloheights.com
-	  </div>
+	  <address>
+	    555 123 4567<br>
+	    info@buffaloheights.com<br>
+	  </address>
 	</div>
 </section>
 
