e34fb77d22519c6861e3abf7e33dc234eafd8f75
Asif Ahmed <asif@stitchdesignco.com>
2017-02-13 11:57:06 -0500

    Add all styles to head section

--- content/themes/bhdDynamic/index.php
+++ content/themes/bhdDynamic/index.php
@@ -20,8 +20,9 @@
 	<header>
 		<p>In the <span>Heart</span> of <span>Houston</span></p>
 	</header>
-	<div><a href="#">Logo</a></div>
+	<div id="head-seal"><a href="#"><?php echo file_get_contents($GLOBALS['url']."/assets/bhd-logo-seal.svg"); ?></a></div>
 	<h1>Buffalo Heights</h1>
+	<div class="underbar"></div>
 	<h2>In the heart of Houston</h2>
 	<button>Watch Video</button>
 </section>
