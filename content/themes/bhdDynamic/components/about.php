c6b8f9386fb4ef54977853c2b77f6f8ec3dcebcc
Asif Ahmed <asif@stitchdesignco.com>
2017-02-13 16:56:00 -0500

    Break up about into a component file

---
+++ content/themes/bhdDynamic/components/about.php
@@ -0,0 +1,39 @@
+<?php
+  /** 
+  * Section => About
+  */ 
+?>
+
+	<div class="chapter">01<div class="dash"></div></div>
+
+	<article id="sub-01">
+		<h1>In the <span>Heart</span> of Houston</h1>
+		<p>Buffalo Heights is a high quality, mixed-use district ideally located in the proximity to Houston's Buffalo Bayou Park, Memorial Park, the Heights, Washington Avenue Corridor and Downtown.</p>
+	</article>
+
+  <div id="hear-houston">
+	  <?php echo file_get_contents($GLOBALS['url']."/assets/heart-houston.svg"); ?>
+  </div>
+
+  <div id="about-slider-01">
+		<img class="about-slider-img" id="" src="<?php echo $GLOBALS['url']."/assets/img-02-mobile.png"; ?>" alt="" />
+		<img class="about-slider-img" id="" src="<?php echo $GLOBALS['url']."/assets/img-02-mobile.png"; ?>" alt="" />
+		<img class="about-slider-img" id="" src="<?php echo $GLOBALS['url']."/assets/img-02-mobile.png"; ?>" alt="" />
+	</div>
+
+	<article id="sub-02">
+		<img class="indented-img" id="" src="<?php echo $GLOBALS['url']."/assets/img-01-mobile.png"; ?>" alt="" />
+		<h1><span>1500</span><sup>+</sup> Acres of urban parkland</h1>
+		<p>This innovative real estate development will offer a complete lifestye built around outdoor activity, healthy local food, and signature greenspace in the region's most desirable and accessible urban infill location. New development and upgrades to existing buildings will enhance proximity to the trails and bayou, and more than 1,500 acres of parkland in the urban core of the city</p>
+  </article>
+
+	<blockquote>
+		<h1>There is no location like this one in Houston</h1>
+		<h2>Russel Gordy Owner of BKR Memorial II, LLC</h2>
+	</blockquote>
+
+	<article id="sub-03">
+	  <img class="" id="" src="<?php echo $GLOBALS['url']."/assets/img-04-mobile.png"; ?>" alt="" />
+	  <h1>Buffalo Heights <span>Phase I</span></h1>
+	  <p>The First phaze of Buffalo Heights is a mixed-use Project on Approximately 4 acres at th intersectio of Washington Avenue and South Heights Blvd. The project includes a new urban prototype H-E-B, multi-family, offices and retail.</p>
+  </article>
