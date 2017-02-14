4bd0da564868991dfb725bba24e7199903ec15a0
Asif Ahmed <asif@stitchdesignco.com>
2017-02-14 17:08:42 -0500

    Change headers and spans for about section

--- content/themes/bhdDynamic/components/about.php
+++ content/themes/bhdDynamic/components/about.php
@@ -7,7 +7,7 @@
 	<div class="chapter">01<div class="dash"></div></div>
 
 	<article id="sub-01">
-		<h1>In the <span>Heart</span> of Houston</h1>
+		<h1>In the <span class="fits">Heart</span> of Houston</h1>
 		<p>Buffalo Heights is a high quality, mixed-use district ideally located in the proximity to Houston's Buffalo Bayou Park, Memorial Park, the Heights, Washington Avenue Corridor and Downtown.</p>
 	</article>
 
@@ -23,7 +23,7 @@
 
 	<article id="sub-02">
 		<img class="indented-img" id="" src="<?php echo $GLOBALS['url']."/assets/img-01-mobile.png"; ?>" alt="" />
-		<h1><span>1500</span><sup>+</sup> Acres of urban parkland</h1>
+		<h1><span class="fits">1500<sup>+</sup></span> Acres of urban parkland</h1>
 		<p>This innovative real estate development will offer a complete lifestye built around outdoor activity, healthy local food, and signature greenspace in the region's most desirable and accessible urban infill location. New development and upgrades to existing buildings will enhance proximity to the trails and bayou, and more than 1,500 acres of parkland in the urban core of the city</p>
   </article>
 
@@ -34,6 +34,6 @@
 
 	<article id="sub-03">
 	  <img class="" id="" src="<?php echo $GLOBALS['url']."/assets/img-04-mobile.png"; ?>" alt="" />
-	  <h1>Buffalo Heights <span>Phase I</span></h1>
+	  <h1>Buffalo Heights <span class="too-long">Phase I</span></h1>
 	  <p>The First phaze of Buffalo Heights is a mixed-use Project on Approximately 4 acres at th intersectio of Washington Avenue and South Heights Blvd. The project includes a new urban prototype H-E-B, multi-family, offices and retail.</p>
   </article>
