9fd8da0b3099697e87e8a64c9de14f76616454b2
Asif Ahmed <asif@stitchdesignco.com>
2017-02-15 15:36:10 -0500

    build a nav menu

--- content/themes/bhdDynamic/components/nav.php
+++ content/themes/bhdDynamic/components/nav.php
@@ -5,4 +5,8 @@
 ?>
 
 <h1>Buffalo Heights</h1>
+<div class="fullmenu" id="nav-01"><a href="#about">About</a></div>
+<div class="fullmenu" id="nav-02"><a href="#press">Press</a></div>
+<div class="fullmenu" id="nav-03"><a href="#leasing">Leasing</a></div>
+<div class="fullmenu" id="nav-04"><a href="#connect">Connect</a></div>
 <div id="hamburger"><?php echo file_get_contents($GLOBALS['url']."/assets/hamburger.svg"); ?></div>
