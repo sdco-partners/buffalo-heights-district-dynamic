398c0b705c7b31b730e69716eb835df37b6aa8b7
Asif Ahmed <asif@stitchdesignco.com>
2017-02-16 04:00:16 -0500

    leftovers from yesterday

--- content/themes/bhdDynamic/components/nav.php
+++ content/themes/bhdDynamic/components/nav.php
@@ -5,8 +5,20 @@
 ?>
 
 <h1>Buffalo Heights</h1>
-<div class="fullmenu" id="nav-01"><a href="#about">About</a></div>
-<div class="fullmenu" id="nav-02"><a href="#press">Press</a></div>
-<div class="fullmenu" id="nav-03"><a href="#leasing">Leasing</a></div>
-<div class="fullmenu" id="nav-04"><a href="#connect">Connect</a></div>
+<div class="fullmenu">
+  <a href="#about">About</a>
+  <div class="chapter-bg">01</div>
+</div>
+<div class="fullmenu">
+  <a href="#press">Press</a>
+  <div class="chapter-bg">02</div>
+</div>
+<div class="fullmenu">
+  <a href="#leasing">Leasing</a>
+  <div class="chapter-bg">03</div>
+</div>
+<div class="fullmenu">
+  <a href="#connect">Connect</a>
+  <div class="chapter-bg">04</div>
+</div>
 <div id="hamburger"><?php echo file_get_contents($GLOBALS['url']."/assets/hamburger.svg"); ?></div>
