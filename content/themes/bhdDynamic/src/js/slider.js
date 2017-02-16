4158feb371cd761bda40e3d3664641576603e4a3
Asif Ahmed <asif@stitchdesignco.com>
2017-02-16 17:49:35 -0500

    build working slider next button

--- content/themes/bhdDynamic/src/js/slider.js
+++ content/themes/bhdDynamic/src/js/slider.js
@@ -3,28 +3,56 @@
 * 
 * Functions related to image sliders
 */
+
+
+
+/**
+* About Slider
+* 
+* Functionality of About Slider
+*/
+
+var aboutSlider = function(){
+
 var counter = 1;
 
-var aboutSlider = function(time, count) {
+  var slide = function(time, count) {
+    // Define vocab
+    var totalImages = $j( '#about-slider-01' ).children().length
+    var firstPosition = $j( '#about-01' ).position().left;
 
-  var totalImages = $j( '#about-slider-01' ).children().length
-  var currentSlide = $j( '#about-0' + count );
-  var currentPosition = currentSlide.position();
-  
-  console.log('data: ', totalImages, count)
+    // If count is within image range, account for indent image and use to move left
+    if ( count <= totalImages ) {
 
-  if ( count < totalImages ) {
-  	$j( ''#about-slider-01'' ).animate({
-  		scrollLeft: currentPosition.left
-  		console.log('position is at: ', currentPosition);
-  	}, time / 2);
-  } else {
-  	counter = 1;
+      var currentSlide = $j( '#about-0' + count );
+      var currentPosition = currentSlide.position().left;
+      var currentWidth = currentSlide.width()
+      
+      var moveTo = currentPosition - firstPosition
+      
+      $j( '#about-slider-01' ).animate({
+        scrollLeft: moveTo
+      }, time / 2 );
+    
+    // Else it's end of slider, reset to initial
+    } else {
+      counter = 1;
+      $j( '#about-slider-01' ).animate({
+        scrollLeft: 0 
+      }, time );
+    }
+
   }
-}
 
-$j( '#next' ).on( 'click', function(){
-	counter += 1;
-	console.log('count is at: ', counter);
-  aboutSlider(5000, counter);
-});
+  $j( '#next' ).on( 'click', function(){
+    counter += 1;
+    console.log('count is at: ', counter);
+    slide(2000, counter);
+  });
+
+  $j( '#prev' ).on( 'click', function(){
+    counter -= 1;
+    console.log('count is at: ', counter);
+    slide(2000, counter);
+  });
+}
