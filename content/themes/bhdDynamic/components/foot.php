3fba6222bc23c838f3a9c8efa38ab4354e343d8a
Asif Ahmed <asif@stitchdesignco.com>
2017-02-16 11:13:37 -0500

    final style polishes on footer and info

--- content/themes/bhdDynamic/components/foot.php
+++ content/themes/bhdDynamic/components/foot.php
@@ -4,26 +4,28 @@
   */ 
 ?>
 
-<header>
-	<p>In the <span>Heart of</span> Houston</p>
-	<div class="underbar"></div>
-</header>
-<div>
+<div id="footer-social">
+  <header>
+    <p>In the <span>Heart of</span> Houston</p>
+    <div class="underbar"></div>
+  </header>
   <ul id="footer-links">
     <li class="links"><?php echo file_get_contents($GLOBALS['url']."/assets/vimeo.svg"); ?></li>
     <li class="links"><?php echo file_get_contents($GLOBALS['url']."/assets/mail.svg"); ?></li>
     <li class="links"><?php echo file_get_contents($GLOBALS['url']."/assets/info.svg"); ?></li>
   </ul>
 </div>
-<div>
-  <h2>Owner</h2>
-  <h1>BKR Memorial II, LLC</h1>
-</div>
-<div>
-  <h2>Developer</h2>
-  <h1>Midway</h1>
-</div>
-<div>
-  <h2>Copyright 2017 Buffalo Heights</h2>
-  <h2>Site by SDCO. Partners</h2>
+<div id="footer-info">
+  <div>
+    <h2>Owner</h2>
+    <h1>BKR Memorial II, LLC</h1>
+  </div>
+  <div>
+    <h2>Developer</h2>
+    <h1>Midway</h1>
+  </div>
+  <div>
+    <h2>Copyright 2017 Buffalo Heights</h2>
+    <h2>Site by SDCO. Partners</h2>
+  </div>
 </div>
