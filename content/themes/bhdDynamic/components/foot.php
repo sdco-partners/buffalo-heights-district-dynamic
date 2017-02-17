7c29c8aa1357e9cd9f582e5dc8f58c2b869803e1
Asif Ahmed <asif@stitchdesignco.com>
2017-02-17 17:48:14 -0500

    Have a blast Benjamin

--- content/themes/bhdDynamic/components/foot.php
+++ content/themes/bhdDynamic/components/foot.php
@@ -10,9 +10,9 @@
     <div class="underbar"></div>
   </header>
   <ul id="footer-links">
-    <li class="links"><?php echo file_get_contents($GLOBALS['url']."/assets/vimeo.svg"); ?></li>
-    <li class="links"><?php echo file_get_contents($GLOBALS['url']."/assets/mail.svg"); ?></li>
-    <li class="links"><?php echo file_get_contents($GLOBALS['url']."/assets/info.svg"); ?></li>
+    <li class="links"><a href="#" alt="" target="_blank"><?php echo file_get_contents($GLOBALS['url']."/assets/vimeo.svg"); ?></a></li>
+    <li class="links"><a href="#" alt="" target="_blank"><?php echo file_get_contents($GLOBALS['url']."/assets/mail.svg"); ?></a></li>
+    <li class="links"><a href="#" alt="" target="_blank"><?php echo file_get_contents($GLOBALS['url']."/assets/info.svg"); ?></a></li>
   </ul>
 </div>
 <div id="footer-info">
@@ -26,6 +26,6 @@
   </div>
   <div>
     <h2>Copyright 2017 Buffalo Heights</h2>
-    <h2>Site by SDCO. Partners</h2>
+    <h2><a href="#" alt="" target="_blank">Site by SDCO. Partners</a></h2>
   </div>
 </div>
