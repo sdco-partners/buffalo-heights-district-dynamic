18f1be05adcbf0b2c657f8ea37b426cbe4fa6996
Asif Ahmed <asif@stitchdesignco.com>
2017-02-17 16:09:39 -0500

    covert margin-right to hard widths

--- content/themes/bhdDynamic/components/press.php
+++ content/themes/bhdDynamic/components/press.php
@@ -7,35 +7,35 @@
 <div class="chapter">02<div class="dash"></div></div>
 <blockquote>
 	<h1>Quote from article quote from Article</h1>
-	<h2>Houston Chronicle
+	<h2>Houston Chronicle<br>
 	January 2017</h2>
   <div class="linkout"><a href="#" alt="">Read More</a></div>
 </blockquote>	
 <div id="press-section">
   <article class="press-box">
     <h1>
-      Houston Chronicle
+      Houston Chronicle<br>
       January 2017
     </h1>
     <div class="linkout"><a href="#" alt="">Read More</a></div>
   </article>
   <article class="press-box">
     <h1>
-      Houston Chronicle
+      Houston Chronicle<br>
       January 2017
     </h1>
     <div class="linkout"><a href="#" alt="">Read More</a></div>
   </article>
   <article class="press-box">
     <h1>
-      Houston Chronicle
+      Houston Chronicle<br>
       January 2017
     </h1>
     <div class="linkout"><a href="#" alt="">Read More</a></div>
   </article>
   <article class="press-box">
     <h1>
-      Houston Chronicle
+      Houston Chronicle<br>
       January 2017
     </h1>
     <div class="linkout"><a href="#" alt="">Read More</a></div>
