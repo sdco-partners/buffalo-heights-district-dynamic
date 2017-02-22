a2dfc3f064a6a60285a53bb7803919f3325b4367
Asif Ahmed <asif@stitchdesignco.com>
2017-02-22 15:18:53 -0500

    Add custom fields to Leasing section

--- content/themes/bhdDynamic/components/about.php
+++ content/themes/bhdDynamic/components/about.php
@@ -30,25 +30,34 @@
     <?php if ( $slider_a ) {
     	$count = 0;
     	foreach ($slider_a as $slide ){ ?>
-			  <img class="about-slider-img" id="about-0<?php echo ++$count ?>" src="<?php echo $slide['url']; ?>" alt="<?php echo $slide['alt']; ?>" />
+			  <img class="about-slider-img" id="about-0<?php echo ++$count ?>" 
+			    src="<?php echo $slide['url']; ?>" 
+			    alt="<?php echo $slide['alt']; ?>" />
 			<?php }
 		} ?>
 	</div>
 
 	<article id="sub-02">
-		<img class="indented-img mobile" id="" src="<?php echo $image_a_mobl['url']; ?>" alt="<?php echo $image_a_mobl['alt']; ?>" />
-		<img class="indented-img desktop" id="" src="<?php echo $image_a_desk['url']; ?>" alt="<?php echo $image_a_desk['alt']; ?>" />
+		<img class="indented-img mobile" id="" 
+		  src="<?php echo $image_a_mobl['url']; ?>" 
+		  alt="<?php echo $image_a_mobl['alt']; ?>" />
+		<img class="indented-img desktop" id="" 
+		  src="<?php echo $image_a_desk['url']; ?>" 
+		  alt="<?php echo $image_a_desk['alt']; ?>" />
 		<h1><span class="fits">1500<sup>+</sup></span> Acres of urban parkland</h1>
 		<p><?php echo get_field('01_copy_b', 2, false); ?></p>
   </article>
 
 	<blockquote>
 		<h1><?php echo get_field('01_quote_a_quote', 2, false); ?></h1>
-		<h2><?php echo get_field('01_quote_a_attr_a', 2, false); ?><br><?php echo get_field('01_quote_a_attr_b', 2, false); ?></h2>
+		<h2><?php echo get_field('01_quote_a_attr_a', 2, false); ?>
+		  <br><?php echo get_field('01_quote_a_attr_b', 2, false); ?></h2>
 	</blockquote>
 
 	<article id="sub-03">
-	  <img class="indents-at-desktop" src="<?php echo $image_b['url']; ?>" alt="<?php echo $image_b['alt']; ?>" />
+	  <img class="indents-at-desktop" 
+	    src="<?php echo $image_b['url']; ?>"
+	    alt="<?php echo $image_b['alt']; ?>" />
 	  <h1>Buffalo Heights <span class="too-long">Phase I</span></h1>
 	  <p><?php echo get_field('01_copy_c', 2, false); ?></p>
   </article>
--- content/themes/bhdDynamic/components/info.php
+++ content/themes/bhdDynamic/components/info.php
@@ -2,11 +2,22 @@
   /** 
   * Section => Info
   */ 
+
+  // Variables
+  $documents = get_field( '03_documents', 2);
 ?>
 
 <div class="chapter">03<div class="dash"></div></div>
 <div>
   <h1>Leasing Information</h1>
-  <div class="linkout"><a href="#" target="_blank" alt="Download Leasing Info">Download Leasing PDF</a></div>
-  <div class="linkout"><a href="#" target="_blank" alt="Download Floor Plans">Download Floor Plans PDF</a></div>
+  <?php if( $documents ){ 
+  	foreach( $documents as $doc ){ 
+  		$file_path = $doc['03_doc_link'];
+  		$file_name = $doc['03_doc_name'];
+  		?>
+      <div class="linkout"><a href="<?php echo $file_path['url']; ?>" target="_blank" alt="<?php echo $file_name; ?>">Download <?php echo $file_name; ?></a></div>
+    <?php	}
+  } ?>
+<!--   <div class="linkout"><a href="#" target="_blank" alt="Download Leasing Info">Download Leasing PDF</a></div>
+  <div class="linkout"><a href="#" target="_blank" alt="Download Floor Plans">Download Floor Plans PDF</a></div> -->
 </div>
