e0a0c222db3c227114b7b3648bf6f592ce365227
Asif Ahmed <asif@stitchdesignco.com>
2017-04-14 16:34:00 -0400

    Add linkable email and telephone

--- content/themes/bhdDynamic/components/connect.php
+++ content/themes/bhdDynamic/components/connect.php
@@ -7,6 +7,10 @@
 <div class="chapter">04<div class="dash"></div></div>
 <h1>Connect</h1>
 <address>
-  <?php echo get_field('phone', 2, false); ?><br>
-  <span id="address-email"><?php echo get_field('email', 2, false); ?></span><br>
+  <a href="tel:<?php echo get_field('phone', 2, false); ?>">
+    <?php echo get_field('phone', 2, false); ?>
+  </a><br>
+  <span id="address-email">
+    <a href="mailto:<?php echo get_field('email', 2, false); ?>"><?php echo get_field('email', 2, false); ?></a>
+  </span><br>
 </address>
